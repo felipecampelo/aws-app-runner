aac606465fd0e890f648e0fbfd1e9ceb9993062c
Felipe Souto Campelo <felipesoutocampelo@gmail.com>
2023-10-30 16:09:16 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,56 @@
+from flask import Flask, request, redirect, url_for, session
 import boto3
-from botocore.exceptions import NoCredentialsError
+import os
 
-# Configuração do pool de usuários
-user_pool_id = 'seu-user-pool-id'
-client_id = 'seu-client-id'
+app = Flask(__name)
+app.secret_key = os.urandom(24)
 
-# Inicialize o cliente Cognito
-cognito_client = boto3.client('cognito-idp')
+# Configuração do Amazon Cognito
+AWS_REGION = os.environ.get('AWS_REGION')
+USER_POOL_ID = os.environ.get('USER_POOL_ID')
+APP_CLIENT_ID = os.environ.get('APP_CLIENT_ID')
+IDENTITY_POOL_ID = os.environ.get('IDENTITY_POOL_ID')
+# AWS_REGION = 'sua-regiao'
+# USER_POOL_ID = 'seu-user-pool-id'
+# APP_CLIENT_ID = 'seu-client-id'
+# IDENTITY_POOL_ID = 'seu-identity-pool-id'
 
-# Autenticação
-username = 'seu-username'
-password = 'sua-senha'
+@app.route('/')
+def home():
+    if 'access_token' in session:
+        return f'Hello, {session["username"]}! <a href="/logout">Logout</a>'
+    else:
+        return 'Bem-vindo! <a href="/login">Login</a>'
 
-try:
-    response = cognito_client.initiate_auth(
-        AuthFlow='USER_PASSWORD_AUTH',
-        AuthParameters={
-            'USERNAME': username,
-            'PASSWORD': password
-        },
-        ClientId=client_id
+@app.route('/login')
+def login():
+    return redirect(f'https://{AWS_REGION}.auth.us-east-1.amazoncognito.com/login?response_type=token&client_id={APP_CLIENT_ID}&redirect_uri={url_for("callback", _external=True)}')
+
+@app.route('/logout')
+def logout():
+    session.pop('access_token', None)
+    session.pop('username', None)
+    return redirect('/')
+
+@app.route('/callback')
+def callback():
+    if 'error' in request.args:
+        return 'Erro durante o login.'
+
+    access_token = request.args['access_token']
+    id_token = request.args['id_token']
+
+    session['access_token'] = access_token
+
+    # Use o SDK da AWS (boto3) para obter informações do usuário
+    client = boto3.client('cognito-idp', region_name=AWS_REGION)
+    response = client.get_user(
+        AccessToken=access_token
     )
 
-    # Você receberá os tokens de autenticação no 'AuthenticationResult' do response
-    print("Tokens de autenticação:", response['AuthenticationResult'])
+    session['username'] = response['Username']
 
-except NoCredentialsError:
-    print("Credenciais AWS não configuradas")
+    return redirect('/')
 
-except Exception as e:
-    print("Erro de autenticação:", e)
-
-
-print('Hello World!')
+if __name__ == '__main__':
+    app.run(debug=True)
